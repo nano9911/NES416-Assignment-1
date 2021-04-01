6bf18fe27b159715c37383e3238d305e765bf9a7
nano9911 <aalkhaldi8@gmail.com>
2021-04-01 08:15:43 +0300

    First commitment in Assignment#1

---
+++ Assignment-2/server/message_handler.h
@@ -0,0 +1,115 @@
+/**
+ * @file message_handler.h
+ * @author Adnan Omar (JUST ID: 123423)
+ * @brief Messages handler header (all from scratch) for NES416/HW1
+ * @date 2021-03-18
+ *
+ */
+
+
+/* Macro to make the function of get values from client message */
+#define var(which,var1,var2)  ((which) == (0) ? (var1) : (var2))
+
+/* Enumeration values to make the operation more understandable */
+enum operations {ERR=0, ADD=1, MINUS=2, MUL=3, DIV=4};
+
+/*
+ *  Just To make the code clearer I separated this part from the message handler.
+ *  It's responssible to determine if this character represents an operation ( + , - , * , / ).
+ */
+enum operations decode_operation(char op)   {
+    enum operations ret = ERR;
+    switch (op) {
+        case '+':
+            ret = ADD;
+            break;
+        case '-':
+            ret = MINUS;
+            break;
+        case '*':
+            ret = MUL;
+            break;
+        case '/':
+            ret = DIV;
+            break;
+        default:
+            ret = ERR;
+    }
+    return ret;
+}
+
+/*
+ *  This function is isolated from main() to keep the networking part clearer. It's
+ *  responsible to handle the function of understanding the client's message, and
+ *  detemine if it's valid or not.
+ *
+ *  If the message is valid it will return 0, and the result will be in passes to 
+ *  the integer result.
+ */
+int handle_msg(char msg[], int len, int *result)
+{
+    int x = 0, y = 0, res = 0;
+    enum operations op = ERR;
+
+    /*
+     *  If a == 0:
+     *      we are filling variable x
+     *  
+     *  else if a == 1:
+     *      we are filling variable y
+     *
+     *  else:
+     *      We have more than one oprator, so return error.
+     */
+    unsigned char a = 0;
+    char temp;
+
+    for (int i = 0; i < len; i++)   {
+        temp = msg[i];
+        if (temp == ' ')    {continue;}
+
+        /* If it's a number add it as digit to the current varibale (x or y) */
+        if (temp >= '0' && temp <= '9')   {
+            /* var() -> is a macro determine which variable we are filling (x or y) */
+            *(var(a, &x, &y)) += (int)temp - 48;
+            *(var(a, &x, &y)) *= 10;
+        }
+
+        else  {
+            /* if it's an operator, we should turn a to > 1, so for the next iterations
+                we could fill the y as the second parametr instead of x. */
+            a++;
+            /* If there's more than one operator return an error. */
+            if (a > 1)  {return -1;}
+            /* If the operator is the last or fisrt character in string, this means that we had 
+                only one value and an operator, so return error. */
+            else if (i == len-1 || i == 0)    {return -1;}
+
+            op = decode_operation(temp);
+            if (op == ERR)    {return -2;}
+        }
+    }
+
+    x /= 10; y /= 10;
+    switch (op) {
+        case ADD:
+            res = x + y;
+            break;
+        case MINUS:
+            res = x - y;
+            break;
+        case MUL:
+            res = x * y;
+            break;
+        case DIV:
+            if (y == 0)
+                return -3;
+            res = x / y;
+            break;
+        default:
+            return -1;
+    }
+
+    *result = res;
+    return 0;
+}
---
+++ Assignment-2/server/server.c
@@ -0,0 +1,133 @@
+/**
+ * @file HW1_server.c
+ * @author Adnan Omar (JUST ID: 123423)
+ * @brief Main server file of NES416/HW2
+ * @date 2021-03-17
+ *
+ */
+
+
+#include "socket_handler.h"
+#include "message_handler.h"
+#include <signal.h>
+
+#define RECV_BUF_LEN 128        // Receive buffer size in bytes
+#define SEND_BUF_LEN 20         // Send buffer size in bytes
+
+int listenfd,               /* Server listening socket */
+    clientfd;               /* Client handler socket */
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)  {
+        printf("Usage: ./calc_server [port no.]\n");
+        exit(1);
+    }
+
+    /*  After fork it will:
+    *   *   Contains children process id in parent.
+    *   *   Will be the last value of it before parent called fork in child .  */
+    int pid = 0;
+
+    /*  hints -> We will fill it with out server specifications
+    *   (ip version, transport protocol, some flags). */
+    struct addrinfo hints;
+
+    /* Will be filled with the address of the peer connection (client ip and port). */
+    struct sockaddr_storage their_addr;
+
+    /* Will contain the length of the returned address from aacept() syetem call. */
+    socklen_t sin_size = sizeof(their_addr);
+
+
+    /*  Firstly, we will clear (zeroing) the hints struct (for safety). Then,
+    *   we will fill it with our configuration of the server.
+    *   AF_INET -> IPv4     |     SOCK_STREAM -> TCP
+    *   AI_PASSIVE -> We will use our this machine ip. */
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_flags = AI_PASSIVE; // use my IP
+
+    /* argv[1] is the port number you want the server to bind and listen to. */
+    CreateSocket(NULL, argv[1], &hints, &listenfd);
+
+    while(1)    {
+        printf("\n\nwaiting for connections...\n\n");
+        memset(&their_addr, 0, sizeof(their_addr));
+
+        /*  accept() will return the client socket to clientfd.
+        *   Also, it will fill their_addr with the IP address and port number of
+        *   the peer of the connecion (client) in network byte order, and the
+        *   length of it in sin_size. */
+        clientfd = accept(listenfd, (struct sockaddr *)&their_addr, &sin_size);
+        
+        if (clientfd == -1)     {
+            perror("accept");
+            break;
+        }
+
+        close(clientfd);  /* We don't need this, so close it.. */
+    }
+
+    close(listenfd);
+    exit(0);
+}
+
+void client_handler(struct sockaddr *their_addr, socklen_t sin_size)
+{
+    close(listenfd);
+
+    char recv_buf[RECV_BUF_LEN], send_buf[SEND_BUF_LEN];
+    int rv, received = 0, result;
+
+    /* Will contain the IP and Port in host byte order of client in their_addr */
+    char peer_name[NI_MAXHOST], peer_port[NI_MAXSERV];
+
+    /*  getnameinfo() will convert the IP:Port from network byte order to
+    *   host byte order.
+    *   The address will be saved as string in host byte order in peer_name
+    *   and peer_port.*/
+    getnameinfo((struct sockaddr *)&their_addr, sin_size, peer_name, sizeof(peer_name),
+                peer_port, sizeof(peer_port), NI_NUMERICHOST | NI_NUMERICSERV);
+    printf("server: got connection from %s:%s\n", peer_name, peer_port);
+
+    while (1) {
+        /* We zeroed (reseted) recv_buf, to clean it for the next data coming. */
+        memset(recv_buf, 0, RECV_BUF_LEN);
+        printf("waiting for client: %s:%s messages...\n", peer_name, peer_port);
+        received = recv(clientfd, recv_buf, RECV_BUF_LEN, 0);
+        if (received <= 0) {
+            perror("server: recv");
+            close(clientfd);
+            break;
+        }
+
+        printf("\nreceived \"%s\" from the client %s:%s\n", recv_buf, peer_name, peer_port);
+
+        if (strcmp(recv_buf, "exit") == 0)   {
+            printf("exiting\n");
+            close(clientfd);
+            break;
+        }
+
+        /* Pass our handler the message. */
+        rv = handle_msg(recv_buf, received, &result);
+
+        if (rv == -1)   {sprintf(send_buf, "Invalid values.");}
+        else if (rv == -2)   {sprintf(send_buf, "Invalid opration.");}
+        else if (rv == -2)   {sprintf(send_buf, "Division by zero.");}
+        else if (rv == 0)    {sprintf(send_buf, "%d", result);}
+
+        rv = send(clientfd, send_buf, strlen(send_buf), 0);
+        printf("Sending \"%s\" to the client\n\n", send_buf);
+        if (rv == -1)   {
+            perror("server: send");
+            close(clientfd);
+            break;
+        }
+    }
+
+    printf("Closing connection with %s:%s\n", peer_name, peer_port);
+   
+}
