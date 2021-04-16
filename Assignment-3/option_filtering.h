ace2b1e8b25f362345d4f7bccc3eebf539eb8c6b
nano9911 <aalkhaldi8@gmail.com>
2021-04-16 14:56:27 +0300

    improved code with set SO_SNDLOWAT and SO_RCVLOWAT

---
+++ Assignment-3/option_filtering.h
@@ -0,0 +1,53 @@
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+
+union val {
+  int				i_val;
+  long				l_val;
+  struct linger		linger_val;
+  struct timeval	timeval_val;
+} val;
+
+static char	*sock_str_flag(union val *, int);
+static char	*sock_str_int(union val *, int);
+static char	*sock_str_linger(union val *, int);
+static char	*sock_str_timeval(union val *, int);
+
+static char	strres[128];
+
+static char	*sock_str_flag(union val *ptr, int sin_size) {
+	if (sin_size != sizeof(int))
+		snprintf(strres, sizeof(strres), "size (%d) not sizeof(int)", sin_size);
+	else
+		snprintf(strres, sizeof(strres), "%s", (ptr->i_val == 0) ? "off" : "on");
+	return(strres);
+}
+
+static char	*sock_str_int(union val *ptr, int sin_size)  {
+	if (sin_size != sizeof(int))
+		snprintf(strres, sizeof(strres), "size (%d) not sizeof(int)", sin_size);
+	else
+		snprintf(strres, sizeof(strres), "%d", ptr->i_val);
+	return(strres);
+}
+
+static char	*sock_str_linger(union val *ptr, int sin_size)   {
+	struct linger	*lptr = &ptr->linger_val;
+
+	if (sin_size != sizeof(struct linger))
+		snprintf(strres, sizeof(strres), "size (%d) not sizeof(struct linger)", sin_size);
+	else
+		snprintf(strres, sizeof(strres), "l_onoff = %d, l_linger = %d", lptr->l_onoff, lptr->l_linger);
+	return(strres);
+}
+
+static char	*sock_str_timeval(union val *ptr, int sin_size)  {
+	struct timeval	*tvptr = &ptr->timeval_val;
+
+	if (sin_size != sizeof(struct timeval))
+		snprintf(strres, sizeof(strres), "size (%d) not sizeof(struct timeval)", sin_size);
+	else
+		snprintf(strres, sizeof(strres), "%d sec, %d usec", tvptr->tv_sec, tvptr->tv_usec);
+	return(strres);
+}
---
+++ Assignment-3/options.h
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>		/* for TCP_xxx defines */
+#include "option_filtering.h"
+
+struct sock_opts {
+  const char *opt_str;
+  int opt_level;
+  int opt_name;
+  char *(*opt_val_str)(union val *, int);
+} sock_opts[] = {
+	/* SOL_SOCKET options */
+    { "SO_BROADCAST",		SOL_SOCKET,	SO_BROADCAST,	sock_str_flag },
+	{ "SO_DEBUG",			SOL_SOCKET,	SO_DEBUG,		sock_str_flag },
+	{ "SO_DONTROUTE",		SOL_SOCKET,	SO_DONTROUTE,	sock_str_flag },
+	{ "SO_ERROR",			SOL_SOCKET,	SO_ERROR,		sock_str_int },
+	{ "SO_KEEPALIVE",		SOL_SOCKET,	SO_KEEPALIVE,	sock_str_flag },
+	{ "SO_LINGER",			SOL_SOCKET,	SO_LINGER,		sock_str_linger },
+	{ "SO_OOBINLINE",		SOL_SOCKET,	SO_OOBINLINE,	sock_str_flag },
+	{ "SO_RCVBUF",			SOL_SOCKET,	SO_RCVBUF,		sock_str_int },
+	{ "SO_SNDBUF",			SOL_SOCKET,	SO_SNDBUF,		sock_str_int },
+	{ "SO_RCVLOWAT",		SOL_SOCKET,	SO_RCVLOWAT,	sock_str_int },
+	{ "SO_SNDLOWAT",		SOL_SOCKET,	SO_SNDLOWAT,	sock_str_int },
+	{ "SO_RCVTIMEO",		SOL_SOCKET,	SO_RCVTIMEO,	sock_str_timeval },
+	{ "SO_SNDTIMEO",		SOL_SOCKET,	SO_SNDTIMEO,	sock_str_timeval },
+	{ "SO_REUSEADDR",		SOL_SOCKET,	SO_REUSEADDR,	sock_str_flag },
+#ifdef	SO_REUSEPORT
+	{ "SO_REUSEPORT",		SOL_SOCKET,	SO_REUSEPORT,	sock_str_flag },
+#else
+	{ "SO_REUSEPORT",		0,			0,				NULL },
+#endif
+	{ "SO_TYPE",			SOL_SOCKET,	SO_TYPE,		sock_str_int },
+#ifdef SO_USELOOPBACK
+	{ "SO_USELOOPBACK",		SOL_SOCKET,	SO_USELOOPBACK,	sock_str_flag },
+#else
+	{ "SO_USELOOPBACK",		0,			0,				NULL },
+#endif
+
+	/* IPPROTO_IP options */
+	{ "IP_TOS",				IPPROTO_IP,	IP_TOS,			sock_str_int },
+	{ "IP_TTL",				IPPROTO_IP,	IP_TTL,			sock_str_int },
+
+
+	/* IPPROTO_IPV6 options */
+#ifdef	IPPROTO_IPV6
+	#ifdef	IPV6_DONTFRAG
+	{ "IPV6_DONTFRAG",		IPPROTO_IPV6,IPV6_DONTFRAG,	sock_str_flag },
+	#else
+	{ "IPV6_DONTFRAG",		0,			0,				NULL },
+	#endif
+	#ifdef	IPV6_UNICAST_HOPS
+	{ "IPV6_UNICAST_HOPS",	IPPROTO_IPV6,IPV6_UNICAST_HOPS,sock_str_int },
+	#else
+	{ "IPV6_UNICAST_HOPS",	0,			0,				NULL },
+	#endif
+	#ifdef	IPV6_V6ONLY
+	{ "IPV6_V6ONLY",		IPPROTO_IPV6,IPV6_V6ONLY,	sock_str_flag },
+	#else
+	{ "IPV6_V6ONLY",		0,			0,				NULL },
+	#endif
+#endif
+
+	/* IPPROTO_TCP options */
+	{ "TCP_MAXSEG",			IPPROTO_TCP,TCP_MAXSEG,		sock_str_int },
+	{ "TCP_NODELAY",		IPPROTO_TCP,TCP_NODELAY,	sock_str_flag },
+
+
+	/* IPPROTO_SCTP options */
+#ifdef IPPROTO_SCTP
+	#ifdef	SCTP_AUTOCLOSE
+	{ "SCTP_AUTOCLOSE",		IPPROTO_SCTP,SCTP_AUTOCLOSE,sock_str_int },
+	#else
+	{ "SCTP_AUTOCLOSE",		0,			0,				NULL },
+	#endif
+	#ifdef	SCTP_MAXBURST
+	{ "SCTP_MAXBURST",		IPPROTO_SCTP,SCTP_MAXBURST,	sock_str_int },
+	#else
+	{ "SCTP_MAXBURST",		0,			0,				NULL },
+	#endif
+	#ifdef	SCTP_MAXSEG
+	{ "SCTP_MAXSEG",		IPPROTO_SCTP,SCTP_MAXSEG,	sock_str_int },
+	#else
+	{ "SCTP_MAXSEG",		0,			0,				NULL },
+	#endif
+	#ifdef	SCTP_NODELAY
+	{ "SCTP_NODELAY",		IPPROTO_SCTP,SCTP_NODELAY,	sock_str_flag },
+	#else
+	{ "SCTP_NODELAY",		0,			0,				NULL },
+	#endif
+	{ NULL,					0,			0,				NULL }
+#endif
+};
