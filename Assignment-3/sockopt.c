e9847d41cb46f1cc37e1a5b324c4eaa2e59c6d89
Adnan Omar Hleihel <aalkhaldi8@gmail.com>
2021-04-17 14:38:28 +0300

    Update sockopt.c

--- Assignment-3/sockopt.c
+++ Assignment-3/sockopt.c
@@ -16,18 +16,49 @@ int main(int argc, char **argv)
 
 	int setsnd=10, setrcv=10, size, getsnd=0, getrcv=0;
 
-	sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0);
-/*
-	if (ioctl(sockfd, FIONBIO, (char *)&on) < 0)	{
-		perror("ioctl(FIONBIO) failed with error");
+	/*************************************************************/
+   	/* Create an AF_INET stream socket, with two versions:	     */
+	/*							     */
+	/* - with SOCK_STREAM | SOCK_NONBLOCK, in this case we won't */
+	/* use ioctl() or fcntl().				     */
+
+	if (sockfd = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, 0) == -1)	{
+		perror("socket AF_INET, SOCK_STREAM | SOCK_NONBLOCK");
+		exit(0);
+	}
+	/*							     */
+	/* - with SOCK_STREAM, in this case we will use ioctl()      */
+	/* or fcntl().						     */
+	if (sockfd = socket(AF_INET, SOCK_STREAM, 0) == -1)	{
+		perror("socket AF_INET, SOCK_STREAM | SOCK_NONBLOCK");
+		exit(0);
+	}
+
+	if (ioctl(sockfd, FIONBIO, (char *)&on) == -1)	{
+		perror("ioctl FIONBIO");
 		printf("errno = %d\n", errno);
 	}
 
-	if (fcntl(sockfd, F_SETFL, fcntl(sockfd, F_GETFL, 0) | O_NONBLOCK) < 0)	{
-		perror("fcntl() failed with error");
+	int flags;
+	if (flags = fcntl(sockfd, F_GETFL, 0) == -1)	{
+		perror("fcntl F_GETFL");
+		printf("errno = %d\n", errno);		
+	}
+//	Linux or FreeBSD:
+	if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)	{
+		perror("fcntl F_SETFL O_NONBLOCK");
 		printf("errno = %d\n", errno);
 	}
-*/
+/*
+//	Solaris:
+	if (fcntl(sockfd, F_SETFL, flags | FNDELAY) == -1)	{
+		perror("fcntl F_SETFL, FNDELAY");
+		printf("errno = %d\n", errno);
+	}
+
+	/* - uncomment one to use the other one			     */
+   	/*************************************************************/
+
 	printf("\nTrying setsockopt(SO_SNDLOWAT):\n");
 	if (setsockopt(sockfd, SOL_SOCKET, SO_SNDLOWAT, &setsnd, sizeof(int)) < 0)	{
 		perror("setsockopt(SO_SNDLOWAT) failed with error");
