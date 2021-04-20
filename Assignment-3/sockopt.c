9cea598fe1874c7826b8288dccedd81aedcfc7e4
nano9911 <aalkhaldi8@gmail.com>
2021-04-20 04:58:29 +0300

    organise, and edit #ifdef macros

--- Assignment-3/sockopt.c
+++ Assignment-3/sockopt.c
@@ -13,9 +13,7 @@
 #include <sys/ioctl.h>
 #include <sys/utsname.h>
 
-// in case of solaris, and you used fcntl(), uncomment this line
-//
-#ifdef SUN
+#if !(defined linux || !defined freebsd)
 #include <unistd.h>
 #include <stropts.h>
 #include <sys/file.h>
@@ -28,7 +26,7 @@ void get_option(struct sock_opts *ptr);
 
 int main(int argc, char **argv)
 {
-	int	sockfd, rv=0, level=-1,setsnd=32, setrcv=128;
+	int	sockfd, rv=0, setsnd=32, setrcv=128;
 	struct sock_opts *ptr;
 	struct utsname buf;
 	
@@ -40,69 +38,50 @@ int main(int argc, char **argv)
 	printf("System info:\n%s %s %s %s %s\n\n", buf.sysname, buf.nodename, buf.release, buf.version, buf.machine);
 
 	/* loop on all options and test if they're available */
-	for (ptr = sock_opts; ptr->opt_str != NULL; ptr++) {
-		if (ptr->opt_level != level)	{
-			level = ptr->opt_level;
-			switch (level)	{
-				case SOL_SOCKET:	printf("SOL_SOCKET:\n"); break;
-				case IPPROTO_IP:	printf("IPPROTO_IP:\n"); break;
-				case IPPROTO_IPV6:	printf("IPPROTO_IPV6:\n"); break;
-				case IPPROTO_TCP:	printf("IPPROTO_TCP:\n"); break;
-				case IPPROTO_SCTP:	printf("IPPROTO_SCTP:\n"); break;
-			}
-		}
+	for (ptr = sock_opts; ptr->opt_str != NULL; ptr++)
+		{get_option(ptr);}
+
+	/*************************************************************/
+   	/* To create a non blocking AF_INET stream socket,           */
+	/* we have two options:                                      */
+	/*                                                           */
+	/* - with SOCK_STREAM | SOCK_NONBLOCK, in this case we won't */
+	/* use ioctl() or fcntl().                                   */
+//	sockfd = socket_nonblock_option();
+	/*                                                           */
+	/*                                                           */
+	/* - with SOCK_STREAM, in this case we will use ioctl()      */
+	sockfd = socket_nonblock_ioctl();
+	/*                                                           */
+	/* or fcntl().                                               */
+//	sockfd = socket_nonblock_fcntl();
+	/*                                                           */
+	/*                                                           */
+	/* Important:                                                */
+	/* Please don't uncomment ioctl() and fctl() in the same     */
+	/* code, I didn't really faced an issue while testing it,    */
+	/* but also I don't know what will go wrong.                 */
+	/*                                                           */
+	/* - uncomment one to use the other one                      */
+   	/*************************************************************/
+
+	if (setsockopt(sockfd, SOL_SOCKET, SO_SNDLOWAT, &setsnd, sizeof(int)) == -1)
+		{perror("\nsetsockopt SOL_SOCKET, SO_SNDLOWAT");}
+	else	{
+		printf("\nsetsockopt SOL_SOCKET, SO_SNDLOWAT: Succeeded\n");
+		printf("Current SO_SNDLOWAT value: ");
+		*ptr = sock_opts[10];
 		get_option(ptr);
 	}
 
-	/*************************************************************/
-   	/* To create a non blocking AF_INET stream socket, 			 */
-	/* we have two options:	     								 */
-	/*							     							 */
-	/* - with SOCK_STREAM | SOCK_NONBLOCK, in this case we won't */
-	/* use ioctl() or fcntl().				     				 */
-//	sockfd = socket_nonblock_option();
-	/*															 */
-	/*															 */
-	/* - with SOCK_STREAM, in this case we will use ioctl()      */
-	sockfd = socket_nonblock_ioctl();
-	/*															 */
-	/* or fcntl().						     					 */
-//	sockfd = socket_nonblock_fcntl();
-	/*															 */
-	/*															 */
-	/* Important:												 */
-	/* Please don't uncomment ioctl() and fctl() in the same 	 */
-	/* code, I didn't really faced an issue while testing it,	 */
-	/* but also I don't know what will go wrong.  			  	 */
-	/*															 */
-	/* - uncomment one to use the other one			     		 */
-   	/*************************************************************/
-
-	printf("\nTrying setsockopt(SO_SNDLOWAT): ");
-	if (setsockopt(sockfd, SOL_SOCKET, SO_SNDLOWAT, &setsnd, sizeof(int)) == -1)	{
-		perror("\nsetsockopt SOL_SOCKET, SO_SNDLOWAT");
-		printf("errno = %d\n", errno);
+	if (setsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &setrcv, sizeof(int)) == -1)
+		{perror("\nsetsockopt SOL_SOCKET, SO_RCVLOWAT");}
+	else	{
+		printf("\nsetsockopt SOL_SOCKET, SO_RCVLOWAT: Succeeded\n");
+		printf("Current SO_RCVLOWAT value: ");
+		*ptr = sock_opts[9];
+		get_option(ptr);
 	}
-	else	{printf("Succeeded\n");}
-
-	/* get SO_SNDLOWAT 											 */
-	printf("Current SO_SNDLOWAT value: ");
-	*ptr = sock_opts[10];
-	get_option(ptr);
-
-	printf("\nTrying setsockopt(SO_RCVLOWAT): ");
-	if (setsockopt(sockfd, SOL_SOCKET, SO_RCVLOWAT, &setrcv, sizeof(int)) == -1)	{
-		perror("\nsetsockopt SOL_SOCKET, SO_RCVLOWAT");
-		printf("errno = %d\n", errno);
-	}
-	else	{printf("Succeeded\n");}
-
-	/*															 */
-	/* get SO_RCVLOWAT											 */
-	printf("Current SO_RCVLOWAT value: ");
-	*ptr = sock_opts[9];
-	get_option(ptr);
-
 	close(sockfd);
 	exit(0);
 }
@@ -125,13 +104,13 @@ int socket_nonblock_ioctl()	{
 		perror("socket AF_INET, SOCK_STREAM");
 		exit(0);
 	}
-#if defined linux || defined freebsd
+#if (defined linux || defined freebsd)
 	if (ioctl(sockfd, FIONBIO, (char *)&on) == -1)	{
 		perror("ioctl FIONBIO, 1");
 		printf("errno = %d\n", errno);
 	}
 	else	{printf("\n\nioctl FIONBIO, 1: succeeded\n");}
-#elif defined SUN
+#else
 	if (ioctl(sockfd, _IONBF, (char *)&on) == -1)	{
 		perror("ioctl _IONBF, 1");
 		printf("errno = %d\n", errno);
@@ -154,7 +133,7 @@ int socket_nonblock_fcntl()	{
 		printf("errno = %d\n", errno);		
 	}
 
-#if defined linux || defined freebsd
+#if (defined linux || defined freebsd)
 	/* In case of Linux or FreeBSD use that if statement, and	 */
 	/* uncomment the next one.									 */
 	if (fcntl(sockfd, F_SETFL, flags | O_NONBLOCK) == -1)	{
@@ -163,7 +142,7 @@ int socket_nonblock_fcntl()	{
 	}
 	else	{printf("\n\nfcntl F_SETFL, O_NONBLOCK: succeeded\n");}
 
-#elif defined solaris
+#else
 	/* In case of Solaris uncomment this if statement, and		 */
 	/* comment out the past if statement						 */
 	if (fcntl(sockfd, F_SETFL, flags | FNDELAY) == -1)	{
@@ -205,7 +184,7 @@ void get_option(struct sock_opts *ptr)	{
 				break;
 		#endif
 			default:
-				sprintf(err, "unknown %d level, from opt_name: %s", ptr->opt_level, ptr->opt_str);
+				sprintf(err, "unknown %d level", ptr->opt_level);
 				perror(err);
 					return;
 			}
